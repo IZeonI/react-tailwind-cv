f78fc7ccfd54e99d69611ceb6b24e1d115f97dc9
IZeonI <osvaldomartinezespinosa@gmail.com>
2026-02-24 16:12:30 -0600

    feat: initialize React project with Vite and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <h1 className="text-8xl font-bold underline">
+        Estamos VIVOOOOOOOOS
+      </h1>
+    </>
+  )
+}
+
+export default App
